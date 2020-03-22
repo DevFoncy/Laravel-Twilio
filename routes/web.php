dda8abd6d0f0e92cdf084013f9863e5c3aad98e8
steveariasarias <steve.arias@unmsm.edu.pe>
2020-03-22 16:00:14 -0500

    Include twilio

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::view('/bulksms', 'bulksms');
+Route::post('/bulksms', 'BulkSmsController@sendSms');
